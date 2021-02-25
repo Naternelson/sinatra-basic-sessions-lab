829c8869ae76f2022daa0a9e929410873317e787
Nate <7nater.nelson@gmail.com>
2021-02-25 15:49:07 -0700

    commit tree 955a2aac16e61a86c0fdf76733111083807c02a6

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        @session = session
+        erb :index
+    end
+
+    post '/checkout' do
+        @item = params[:item]
+        session[:item] = @item
+        @session = session
+        erb :checkout
+    end
 end
